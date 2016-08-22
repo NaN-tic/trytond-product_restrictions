c723b0a5e50fe874bd789fbc4c5183d3e4e3e483
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-22 10:50:05 +0200

    Upgrade test to version 4.0

--- tests/test_product_restrictions.py
+++ tests/test_product_restrictions.py
@@ -2,87 +2,77 @@
 # copyright notices and license terms.
 from decimal import Decimal
 import unittest
-from trytond.error import UserError
-from trytond.transaction import Transaction
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
+from trytond.error import UserError
+from trytond.pool import Pool
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
+    module = 'product_restrictions'
 
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('product_restrictions')
-
-    def test0005views(self):
-        'Test views'
-        test_view('product_restrictions')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
-
+    @with_transaction()
     def test_restrictions(self):
         'Test restrictions'
-        Party = POOL.get('party.party')
-        Template = POOL.get('product.template')
-        Restriction = POOL.get('product.restriction')
-        Uom = POOL.get('product.uom')
+        pool = Pool()
+        Party = pool.get('party.party')
+        Template = pool.get('product.template')
+        Restriction = pool.get('product.restriction')
+        Uom = pool.get('product.uom')
 
-        with Transaction().start(DB_NAME, USER, context=CONTEXT):
-            restriction = Restriction(name='Inflamable Products')
-            restriction.save()
-            parties = Party.create([{
-                        'name': 'Not Restricted Party',
-                        }, {
-                        'name': 'Supplier',
-                        'supplier_restrictions': [('add', [restriction.id])],
-                        }, {
-                        'name': 'Customer',
-                        'customer_restrictions': [('add', [restriction.id])],
-                        }])
-            free_party, supplier, customer = parties
-            unit, = Uom.search([('symbol', '=', 'u')])
-            free_template, restricted_template = Template.create([{
-                        'name': 'Free Template',
-                        'default_uom': unit,
-                        'list_price': Decimal(0),
-                        'cost_price': Decimal(0),
-                        }, {
-                        'name': 'Restricted Template',
-                        'default_uom': unit,
-                        'list_price': Decimal(0),
-                        'cost_price': Decimal(0),
-                        'restrictions': [('add', [restriction.id])],
-                        }])
+        restriction = Restriction(name='Inflamable Products')
+        restriction.save()
+        parties = Party.create([{
+                    'name': 'Not Restricted Party',
+                    }, {
+                    'name': 'Supplier',
+                    'supplier_restrictions': [('add', [restriction.id])],
+                    }, {
+                    'name': 'Customer',
+                    'customer_restrictions': [('add', [restriction.id])],
+                    }])
+        free_party, supplier, customer = parties
+        unit, = Uom.search([('symbol', '=', 'u')])
+        free_template, restricted_template = Template.create([{
+                    'name': 'Free Template',
+                    'default_uom': unit,
+                    'list_price': Decimal(0),
+                    'cost_price': Decimal(0),
+                    }, {
+                    'name': 'Restricted Template',
+                    'default_uom': unit,
+                    'list_price': Decimal(0),
+                    'cost_price': Decimal(0),
+                    'restrictions': [('add', [restriction.id])],
+                    }])
 
-            for party in parties:
-                Template.check_product_restrictions([free_template], party)
-                Template.check_product_restrictions([free_template], party,
-                        type='supplier')
+        for party in parties:
+            Template.check_product_restrictions([free_template], party)
+            Template.check_product_restrictions([free_template], party,
+                    type='supplier')
 
-            with self.assertRaises(UserError):
-                Template.check_product_restrictions([restricted_template],
-                    free_party)
-
-            with self.assertRaises(UserError):
-                Template.check_product_restrictions([restricted_template],
-                    free_party, type='supplier')
-
-            templates = [free_template, restricted_template]
-            for party in [free_party, supplier]:
-                with self.assertRaises(UserError):
-                    Template.check_product_restrictions(templates, party)
-            for party in [free_party, customer]:
-                with self.assertRaises(UserError):
-                    Template.check_product_restrictions(templates, party,
-                        type='supplier')
-
+        with self.assertRaises(UserError):
             Template.check_product_restrictions([restricted_template],
-                customer)
+                free_party)
+
+        with self.assertRaises(UserError):
             Template.check_product_restrictions([restricted_template],
-                supplier, type='supplier')
+                free_party, type='supplier')
+
+        templates = [free_template, restricted_template]
+        for party in [free_party, supplier]:
+            with self.assertRaises(UserError):
+                Template.check_product_restrictions(templates, party)
+        for party in [free_party, customer]:
+            with self.assertRaises(UserError):
+                Template.check_product_restrictions(templates, party,
+                    type='supplier')
+
+        Template.check_product_restrictions([restricted_template],
+            customer)
+        Template.check_product_restrictions([restricted_template],
+            supplier, type='supplier')
 
 
 def suite():
